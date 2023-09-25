fa26c59db10c0bbe3f9ed9eca03f299e62d7cf7a
Leonardo Dagnino <leodag.sch@gmail.com>
2023-09-25 11:15:07 -0300

    rename to form-feed-st

--- form-feed-st.el
+++ form-feed-st.el
@@ -1,12 +1,12 @@
-;;; ff-st.el --- Display ^L glyphs as horizontal lines -*- lexical-binding: t -*-
+;;; form-feed-st.el --- Display ^L glyphs as horizontal lines  -*- lexical-binding: t -*-
 
 ;; Author: Leonardo Schripsema
 ;; Version: 0.2.0
 ;; Package-Requires: ((emacs "25.1"))
-;; URL: https://github.com/leodag/ff-st
+;; URL: https://github.com/leodag/form-feed-st
 ;; Keywords: faces
 
-;; Copyright (C) 2020-2021 Leonardo Schripsema
+;; Copyright (C) 2020-2023 Leonardo Schripsema
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@
 ;; intended to be used that way.  The following snippet would enable
 ;; it for Emacs Lisp files for instance:
 ;;
-;;     (add-hook 'emacs-lisp-mode-hook 'ff-st-mode)
+;;     (add-hook 'emacs-lisp-mode-hook 'form-feed-st-mode)
 
 ;; Forked from Vasilij Schneidermann's form-feed:
 ;; https://depp.brause.cc/form-feed/
@@ -50,108 +50,108 @@
 
 ;;; Variables
 
-(defgroup ff-st nil
+(defgroup form-feed-st nil
   "Turn ^L glyphs into horizontal lines."
-  :prefix "ff-st-"
+  :prefix "form-feed-st-"
   :group 'faces)
 
-(defface ff-st-line
+(defface form-feed-st-line
   '((((type graphic)) :strike-through t :extend t)
     (((type tty)) :inherit font-lock-comment-face :underline t :extend t))
-  "Face for ff-st-mode lines."
-  :group 'ff-st)
+  "Face for form-feed-st-mode lines."
+  :group 'form-feed-st)
 
-(defcustom ff-st-extra-properties nil
+(defcustom form-feed-st-extra-properties nil
   "List of additional text properties to add to form feeds."
   :type '(plist)
-  :group 'ff-st)
+  :group 'form-feed-st)
 
-(defvar-local ff-st--font-lock-keywords nil
+(defvar-local form-feed-st--font-lock-keywords nil
   "Font-lock keywords to be removed when the mode is disabled.")
 
-(defcustom ff-st-lighter " ^L"
-  "Lighter for `ff-st-mode'."
+(defcustom form-feed-st-lighter " ^L"
+  "Lighter for `form-feed-st-mode'."
   :type 'string
-  :group 'ff-st)
+  :group 'form-feed-st)
 
 
 ;;; Functions
 
-(defun ff-st--add-font-lock-keywords ()
+(defun form-feed-st--add-font-lock-keywords ()
   "Add buffer-local keywords to display page delimiter lines.
 Make sure the special properties involved get cleaned up on
-removal of the keywords via `ff-st-remove-font-lock-keywords'."
+removal of the keywords via `form-feed-st-remove-font-lock-keywords'."
   (make-local-variable 'font-lock-extra-managed-props)
-  (setq ff-st--font-lock-keywords
-        `((,(concat page-delimiter ".*\n?") 0 '(face ff-st-line ,@ff-st-extra-properties) t)
+  (setq form-feed-st--font-lock-keywords
+        `((,(concat page-delimiter ".*\n?") 0 '(face form-feed-st-line ,@form-feed-st-extra-properties) t)
           (,page-delimiter 0 '(face nil display (space :width 2)))))
-  (dolist (prop `(display ,@ff-st-extra-properties))
+  (dolist (prop `(display ,@form-feed-st-extra-properties))
     (cl-pushnew prop font-lock-extra-managed-props))
-  (font-lock-add-keywords nil ff-st--font-lock-keywords t))
+  (font-lock-add-keywords nil form-feed-st--font-lock-keywords t))
 
-(defun ff-st--remove-font-lock-keywords ()
+(defun form-feed-st--remove-font-lock-keywords ()
   "Remove buffer-local keywords displaying page delimiter lines."
-  (font-lock-remove-keywords nil ff-st--font-lock-keywords))
+  (font-lock-remove-keywords nil form-feed-st--font-lock-keywords))
 
-(defun ff-st--on ()
+(defun form-feed-st--on ()
   "Turn on the mode and manage font-lock keywords."
-  (ff-st--add-font-lock-keywords)
+  (form-feed-st--add-font-lock-keywords)
   (font-lock-flush))
 
-(defun ff-st--off ()
+(defun form-feed-st--off ()
   "Turn off the mode and manage font-lock keywords."
-  (ff-st--remove-font-lock-keywords)
+  (form-feed-st--remove-font-lock-keywords)
   (font-lock-flush))
 
 
 ;;; Minor mode
 
 ;;;###autoload
-(define-minor-mode ff-st-mode
-  "Toggle ff-st-mode.
+(define-minor-mode form-feed-st-mode
+  "Toggle form-feed-st-mode.
 
 This minor mode displays page delimiters which usually appear as ^L
 glyphs on a single line as horizontal lines spanning the entire
 window."
-  :lighter ff-st-lighter
-  :group 'ff-st
-  :require 'ff-st
-  (if ff-st-mode
-      (ff-st--on)
-    (ff-st--off)))
+  :lighter form-feed-st-lighter
+  :group 'form-feed-st
+  :require 'form-feed-st
+  (if form-feed-st-mode
+      (form-feed-st--on)
+    (form-feed-st--off)))
 
 
 ;;; Global mode
 
-(defcustom ff-st-include-modes '(prog-mode text-mode)
-  "Major modes in which `ff-st-mode' is activated.
-This is used by `global-ff-st-mode' which activates `ff-st-mode'
+(defcustom form-feed-st-include-modes '(prog-mode text-mode)
+  "Major modes in which `form-feed-st-mode' is activated.
+This is used by `global-form-feed-st-mode' which activates `form-feed-st-mode'
 in all buffers whose major mode derives from one of the modes
 listed here, but not from one of the modes listed in
-`ff-st-exclude-modes'."
+`form-feed-st-exclude-modes'."
   :type '(repeat function)
-  :group 'ff-st)
+  :group 'form-feed-st)
 
-(defcustom ff-st-exclude-modes nil
-  "Major modes in which `ff-st-mode' is not activated.
-This is used by `global-ff-st-mode' which activates `ff-st-mode'
+(defcustom form-feed-st-exclude-modes nil
+  "Major modes in which `form-feed-st-mode' is not activated.
+This is used by `global-form-feed-st-mode' which activates `form-feed-st-mode'
 in all buffers whose major mode derives from one of the modes
-listed in `ff-st-include-modes', but not from one of the modes
+listed in `form-feed-st-include-modes', but not from one of the modes
 listed here."
   :type '(repeat function)
-  :group 'ff-st)
+  :group 'form-feed-st)
 
-(defun ff-st-mode-maybe ()
+(defun form-feed-st-mode-maybe ()
   "Turn on function for the mode."
-  (when (and (apply #'derived-mode-p ff-st-include-modes)
-             (not (apply #'derived-mode-p ff-st-exclude-modes))
+  (when (and (apply #'derived-mode-p form-feed-st-include-modes)
+             (not (apply #'derived-mode-p form-feed-st-exclude-modes))
              (not (bound-and-true-p enriched-mode)))
-    (ff-st-mode)))
+    (form-feed-st-mode)))
 
 ;;;###autoload
-(define-global-minor-mode global-ff-st-mode
-  ff-st-mode ff-st-mode-maybe)
+(define-global-minor-mode global-form-feed-st-mode
+  form-feed-st-mode form-feed-st-mode-maybe)
 
-(provide 'ff-st)
+(provide 'form-feed-st)
 
-;;; ff-st.el ends here
+;;; form-feed-st.el ends here
